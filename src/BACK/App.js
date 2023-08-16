e85cb52eae883fcf9a124a022f94a56ce593d915
Juli <julianmoring5@gmail.com>
2023-08-16 16:45:42 +0100

    get basico

--- src/BACK/App.js
+++ src/BACK/App.js
@@ -1,13 +1,20 @@
 // Importaciones
+const { getPoolConnection } = require('./connectionDB.js')
 const express = require('express')
 const app = express()
 require('dotenv').config()
-
+// Variables de entorno
 const PORT = process.env.HOST_PORT
 
-app.get('/', (req, res) => {
-  console.log('funciona')
-  res.send('funciona')
+app.use(express.json())
+
+// Primero que nos devuelva lo que tenga en la base de datos para mostrarlo en la pantalla
+app.get('/', async (req, res) => {
+  const pool = await getPoolConnection()
+
+  const [recetas] = await pool.query('SELECT * FROM recetas')
+
+  res.status(200).json({ recetas })
 })
 
 app.listen(PORT, () => {
