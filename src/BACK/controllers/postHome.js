7b17b431d6e41d5c15fc3eafa1e2ae9b501cd317
Juli <julianmoring5@gmail.com>
2023-08-17 17:04:44 +0100

    Avances en el postHome

--- src/BACK/controllers/postHome.js
+++ src/BACK/controllers/postHome.js
@@ -1,4 +1,5 @@
 const { getPoolConnection } = require('../connectionDB.js')
+const createError = require('../../../helpers/defaultError.js')
 const { v4: uuidv4 } = require('uuid')
 
 async function postHome (req, res, next) {
@@ -9,19 +10,20 @@ async function postHome (req, res, next) {
     const pool = await getPoolConnection()
     const recetas = await pool.query('SELECT * FROM recetas')
 
-    // const repeatName = recetas[0].filter((e) => {
-    //   return e.nombre === name
-    // })
+    const repeatName = recetas[0].filter((e) => {
+      return e.nombre.toLowerCase() === name.toLowerCase()
+    })
 
-    // if(repeatName){
-    //   return
-    // }
+    if (repeatName.length > 0) {
+      throw createError(400, 'Ya existe una receta con este nombre. Introduce un nombre diferente.')
+    }
+
     await pool.query(
       'INSERT INTO recetas (id, nombre, ingredientes, comensales, descripcion) VALUES (?, ?, ?, ?, ?)',
       [id, name, ingredients, persons, description]
     )
 
-    res.status(200).send('Receta agregada con exito')
+    res.status(200).send('Receta agregada con éxito')
   } catch (error) {
     next(error)
   }
